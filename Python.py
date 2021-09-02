c063fe9fac4705048375c6cccbcbb10dafaac634
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-09-02 12:07:36 -0400

    Added standardized decryption method for python

--- Python.py
+++ Python.py
@@ -9,6 +9,12 @@ class SynAES:
         ciphertxt,tag = cipher.encrypt_and_digest(data.encode("utf-8"))
         return base64.b64encode(ciphertxt+tag).decode("utf-8")
     def decrypt(self,data,iv):
+        data = base64.b64decode(data)
+        cipher = AES.new(self.key,AES.MODE_GCM,nonce=iv.encode("utf-8"))
+        plaintxt = cipher.decrypt(data[:len(data)-16])
+        cipher.verify(data[-16:])
+        return plaintxt.decode("utf-8")
+    def tagdecrypt(self,data,iv):
         # Encryption Method
         data = base64.b64decode(data)
         cipher = AES.new(self.key,AES.MODE_GCM,nonce=iv.encode("utf-8"))
@@ -29,3 +35,7 @@ class SynAES:
 crypt = SynAES("0123456789ABCDEF0123456789ABCDEF")
 print(crypt.encrypt("YO SUP","0123456789AB"))
 print(crypt.decrypt("lyiAqsYTgCuWO0WcO7HKJ8IqZwwZWg==","0123456789AB"))
+print(crypt.tagdecrypt("lyiAqsYTgCuWO0WcO7HKJ8IqZwwZWg==","0123456789AB"))
+# Difference between the decrypt methods is that regular decrypt will only return data and throws an error when tag is mismatched
+# tagdecrypt on the other hand will handle it for you and returns a table containing both the tag's validation status and the data
+# tagdecrypt allows you to use the data even if the tag validation fails but I suggest you NOT to because the data is likely tampered

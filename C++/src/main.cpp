cf199b62d5818b76d7692133ae53f1bb1576f37b
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-09-05 22:38:24 -0400

    Completed C++

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <cstring>
 #include <string.h>
+#include <vector>
 
 SynAES::SynAES(std::string MainKey) {
     std::copy(MainKey.begin(),MainKey.end(),key);
@@ -30,28 +31,27 @@ std::string SynAES::encrypt(std::string Data, std::string IV) {
     EVP_EncryptUpdate(ctx, CipherTxt, &CipherSize, (unsigned char*)Data.c_str(), strlen(Data.c_str()));
     EVP_EncryptFinal_ex(ctx, CipherTxt, &CipherSize);
     EVP_CIPHER_CTX_ctrl(ctx,EVP_CTRL_AEAD_GET_TAG, 16, &CipherTxt[strlen(Data.c_str())]);
-    //CipherTxt[CipherSize+16] = 0;
     EVP_CIPHER_CTX_free(ctx);
     return base64_encode(CipherTxt,sizeof(CipherTxt));
-    
 }
-
+void specstrcpy(char* dest,const char* src, int len) {
+    for(int i=0;i<len;i++) {
+        dest[i] = src[i];
+    }
+}
 std::string SynAES::decrypt(std::string Data, std::string IV) {
-    const unsigned char* MainIV = reinterpret_cast<const unsigned char*>(IV.c_str());
-    const unsigned char* MainDataAndTag = const_cast<const unsigned char*>(base64_decode(Data).data());
-    const unsigned char* MainData = new unsigned char[1000]; // 
-    const unsigned char* MainTag = new unsigned char[1000];
-    std::memcpy(const_cast<unsigned char*>(MainDataAndTag),&MainData,strlen((const char*)MainDataAndTag)-16);
-    std::memcpy(const_cast<unsigned char*>(&MainDataAndTag[strlen((const char*)MainDataAndTag)-16]),MainTag,16);
-    //delete[] MainDataAndTag;
+    std::vector<BYTE> MainDataAndTag = base64_decode(Data);
+    const unsigned char* ConstDataTag = MainDataAndTag.data();
+    unsigned char* MainData = new unsigned char[strlen((const char*)ConstDataTag)-16];
+    unsigned char* MainTag = new unsigned char[16];
+    std::strncpy(reinterpret_cast<char*>(MainData),reinterpret_cast<const char*>(ConstDataTag),strlen((const char*)ConstDataTag)-16); //const_cast
+    std::strncpy(reinterpret_cast<char*>(MainTag),reinterpret_cast<const char*>(&ConstDataTag[strlen((const char*)ConstDataTag)-16]),16);
     EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    EVP_DecryptInit_ex(ctx, CipherType, NULL, NULL, NULL);
-    EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_SET_IVLEN, sizeof(MainIV), NULL);
-    EVP_DecryptInit_ex(ctx, NULL, NULL, key, MainIV);
-    unsigned char PlainTxt[sizeof(MainData)];
+    EVP_DecryptInit_ex(ctx, CipherType, NULL, key, (unsigned char*)IV.c_str());
+    unsigned char PlainTxt[strlen((const char*)MainData)];
     int OutputLen;
-    EVP_DecryptUpdate(ctx, PlainTxt, &OutputLen, MainData, sizeof(MainData));
-    EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_SET_TAG, 16, (void *)MainTag);
+    EVP_DecryptUpdate(ctx, PlainTxt, &OutputLen, const_cast<const unsigned char *>(MainData), strlen((const char*)MainData));
+    EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_SET_TAG, 16, (void*)MainTag);
     int TagValid = EVP_DecryptFinal_ex(ctx, PlainTxt, &OutputLen);
     EVP_CIPHER_CTX_free(ctx);
     delete[] MainData;
@@ -67,7 +67,7 @@ std::string SynAES::decrypt(std::string Data, std::string IV) {
 int main() {
     SynAES * crypt = new SynAES("0123456789ABCDEF0123456789ABCDEF");
     std::cout << "Encryption: " << crypt->encrypt("YO SUP","0123456789AB").c_str() << std::endl;
-    //std::cout << "Decryption: " << crypt->decrypt("lyiAqsYTgCuWO0WcO7HKJ8IqZwwZWg==","0123456789AB").c_str() << std::endl;
+    std::cout << "Decryption: " << crypt->decrypt("lyiAqsYTgCuWO0WcO7HKJ8IqZwwZWg==","0123456789AB").c_str() << std::endl;
     std::string Pause;
     std::cin >> Pause;
 }

0608edf237a96a53b7ecc5cdcc75488a7a82f4db
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-09-06 14:39:43 -0400

    Compiler check for dll export

--- C++/src/SynAES/SynAES.h
+++ C++/src/SynAES/SynAES.h
@@ -3,9 +3,21 @@
 #include <openssl/bio.h>
 #include <openssl/evp.h>
 
-#define DllExport __declspec(dllexport)
-
-class DllExport SynAES {
+#if defined(_MSC_VER)
+    //  Microsoft 
+    #define EXPORT __declspec(dllexport)
+    #define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+    //  GCC
+    #define EXPORT __attribute__((visibility("default")))
+    #define IMPORT
+#else
+    //  do nothing and hope for the best?
+    #define EXPORT
+    #define IMPORT
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+class EXPORT SynAES {
     public:
       SynAES(std::string Key);
       std::string encrypt(std::string Data, std::string IV);

7b79b4bbcf5ffd1f107976e94a83642e59c16197
profess1onal <44492914+profess1onal@users.noreply.github.com>
2022-05-05 18:47:15 +0100

    fix
    fixed so it doesnt give the error:
    SyntaxError: Unexpected token =

--- NodeJS.js
+++ NodeJS.js
@@ -1,34 +1,34 @@
 const crypto = require("crypto")
 class SynAES {
-  constructor(key) {
-    this.key = Buffer.from(key,'utf-8')
-	switch(Buffer.byteLength(key)) {
-		case 16:
-			this.gcmalg = "aes-128-gcm"
-			break;
-		case 24:
-			this.gcmalg = "aes-192-gcm"
-			break;
-		case 32:
-			this.gcmalg = "aes-256-gcm"
-			break;
-		default:
-			console.error("Invalid Key Length")
-			return;
-	}
-  }
-  encrypt = function(data,iv) {
-    const cipher = crypto.createCipheriv(this.gcmalg, this.key, iv)
-    const encryptedBuffer = Buffer.concat([cipher.update(data), cipher.final()])
-    return Buffer.concat([encryptedBuffer,cipher.getAuthTag()]).toString("base64")
-  }
-  decrypt = function(data,iv) {
-    data = Buffer.from(data,'base64');
-    const datalen = Buffer.byteLength(data);
-    const decipher = crypto.createDecipheriv(this.gcmalg, this.key, iv)
-    decipher.setAuthTag(data.slice(datalen-16,datalen))
-    return Buffer.concat([decipher.update(data.slice(0,datalen-16)),decipher.final()]).toString()
-  }
+    constructor(key) {
+        this.key = Buffer.from(key, 'utf-8')
+        switch (Buffer.byteLength(key)) {
+            case 16:
+                this.gcmalg = "aes-128-gcm"
+                break;
+            case 24:
+                this.gcmalg = "aes-192-gcm"
+                break;
+            case 32:
+                this.gcmalg = "aes-256-gcm"
+                break;
+            default:
+                console.error("Invalid Key Length")
+                return;
+        }
+    }
+    encrypt(data = '', iv = '')  {
+        const cipher = crypto.createCipheriv(this.gcmalg, this.key, iv)
+        const encryptedBuffer = Buffer.concat([cipher.update(data), cipher.final()])
+        return Buffer.concat([encryptedBuffer, cipher.getAuthTag()]).toString("base64")
+    }
+    decrypt(data = '', iv = '')  {
+        data = Buffer.from(data, 'base64');
+        const datalen = Buffer.byteLength(data);
+        const decipher = crypto.createDecipheriv(this.gcmalg, this.key, iv)
+        decipher.setAuthTag(data.slice(datalen - 16, datalen))
+        return Buffer.concat([decipher.update(data.slice(0, datalen - 16)), decipher.final()]).toString()
+    }
 }
 
 // Example Usages

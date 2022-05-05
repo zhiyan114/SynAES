47a011c39706b0b79b64c0650cb443a70895a9ec
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2022-05-05 15:45:52 -0400

    Added TypeScript module

--- Typescript.ts
+++ Typescript.ts
@@ -1,6 +1,9 @@
-const crypto = require("crypto")
-class SynAES {
-    constructor(key) {
+import { assert } from 'console';
+import { CipherGCMTypes, createCipheriv, createDecipheriv } from 'crypto';
+export default class SynAES {
+    private key: Buffer;
+    private gcmalg: CipherGCMTypes;
+    constructor(key: string) {
         this.key = Buffer.from(key, 'utf-8')
         switch (Buffer.byteLength(key)) {
             case 16:
@@ -16,21 +19,25 @@ class SynAES {
                 throw new Error("Invalid Key Length: 16, 24, 32")
         }
     }
-    encrypt(data = '', iv = '')  {
-        const cipher = crypto.createCipheriv(this.gcmalg, this.key, iv)
+    encrypt(data : string, iv : string) : string  {
+        assert(iv.length == 12, "IV must be 12 bytes")
+        const cipher = createCipheriv(this.gcmalg, this.key, iv);
         const encryptedBuffer = Buffer.concat([cipher.update(data), cipher.final()])
         return Buffer.concat([encryptedBuffer, cipher.getAuthTag()]).toString("base64")
     }
-    decrypt(data = '', iv = '')  {
-        data = Buffer.from(data, 'base64');
-        const datalen = Buffer.byteLength(data);
-        const decipher = crypto.createDecipheriv(this.gcmalg, this.key, iv)
-        decipher.setAuthTag(data.slice(datalen - 16, datalen))
-        return Buffer.concat([decipher.update(data.slice(0, datalen - 16)), decipher.final()]).toString()
+    decrypt(data : string, iv : string) : string {
+        let byteData = Buffer.from(data, 'base64');
+        const datalen = Buffer.byteLength(byteData);
+        const decipher = createDecipheriv(this.gcmalg, this.key, iv)
+        decipher.setAuthTag(byteData.slice(datalen - 16, datalen))
+        return Buffer.concat([decipher.update(byteData.slice(0, datalen - 16)), decipher.final()]).toString()
     }
 }
 
+/*
 // Example Usages
+import {SynAES} from "./Typescript"
 const crypt = new SynAES("0123456789ABCDEF0123456789ABCDEF")
 console.log(crypt.decrypt("lyiAqsYTgCuWO0WcO7HKJ8IqZwwZWg==","0123456789AB"))
 console.log(crypt.encrypt("YO SUP","0123456789AB"))
+*/

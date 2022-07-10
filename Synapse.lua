8a6c6668c7cdf25877e0238c32add2daf561d9d7
profess1onal <44492914+profess1onal@users.noreply.github.com>
2022-07-10 21:02:46 +0100

    fix unused var

--- Synapse.lua
+++ Synapse.lua
@@ -1,17 +1,17 @@
 -- Main Usage
 --Decryption:
-print(syn.crypt.custom.decrypt("aes-gcm","lyiAqsYTgCuWO0WcO7HKJ8IqZwwZWg==","0123456789ABCDEF0123456789ABCDEF","0123456789AB"))
+print(syn.crypt.custom.decrypt("aes-gcm", "lyiAqsYTgCuWO0WcO7HKJ8IqZwwZWg==", "0123456789ABCDEF0123456789ABCDEF", "0123456789AB"))
 --Encryption:
-print(syn.crypt.custom.encrypt("aes-gcm","YO SUP","0123456789ABCDEF0123456789ABCDEF","0123456789AB"))
+print(syn.crypt.custom.encrypt("aes-gcm", "YO SUP", "0123456789ABCDEF0123456789ABCDEF", "0123456789AB"))
 
 -- Constructor format
 local SynAES = function(key)
     local crypto = syn.crypt.custom
     local enc = function(data,iv)
-        return syn.crypt.custom.encrypt("aes-gcm",data,key,iv)
+        return crypto.encrypt("aes-gcm", data, key, iv)
     end
     local dec = function(data,iv)
-        return syn.crypt.custom.decrypt("aes-gcm",data,key,iv)
+        return crypto.decrypt("aes-gcm", data, key, iv)
     end
     return {
         encrypt=enc,
@@ -21,5 +21,5 @@ end
 
 -- Example Usages
 local crypt = SynAES("0123456789ABCDEF0123456789ABCDEF")
-print(crypt.encrypt("YO SUP","0123456789AB"))
-print(crypt.decrypt("lyiAqsYTgCuWO0WcO7HKJ8IqZwwZWg==","0123456789AB"))
+print(crypt.encrypt("YO SUP", "0123456789AB"))
+print(crypt.decrypt("lyiAqsYTgCuWO0WcO7HKJ8IqZwwZWg==", "0123456789AB"))
